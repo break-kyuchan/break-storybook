c0818a5379f2eff7a1b92bdd1fdc90425d2c776e
김효선 <kimhyosun@gimhyoseon-ui-MacBookPro.local>
2021-06-02 13:34:18 +0900

    prettier setting

--- src/Components/Header/Atoms/Button.tsx
+++ src/Components/Header/Atoms/Button.tsx
@@ -1,27 +1,21 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
-export interface ButtonProps{
-    title?: string;
+export interface ButtonProps {
+  title?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-    title="HOME",
-})=>{
-return (
-    <ButtonContainer>
-        {title}
-    </ButtonContainer>
-)
-}
+export const Button: React.FC<ButtonProps> = ({ title = 'HOME' }) => {
+  return <ButtonContainer>{title}</ButtonContainer>;
+};
 
-const ButtonContainer=styled.div`
-height:20px;
-font-size : 14px;
-font-weight : 900;
-color: #000000;
-margin : 0 15px;
-&:hover{
-    cursor : pointer;
-}
-`
+const ButtonContainer = styled.div`
+  height: 20px;
+  font-size: 14px;
+  font-weight: 900;
+  color: #000000;
+  margin: 0 15px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
